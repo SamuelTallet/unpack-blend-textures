8ba23d8286344959ee54f19a177b619c5c0b15ac
Samuel Tallet <samuel.tallet@gmail.com>
2023-05-26 13:50:05 +0200

    Support all image formats

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::path::Path;
 use std::fs::metadata as file_meta;
 use std::fs::create_dir;
 use std::fs::File;
@@ -28,35 +29,29 @@ fn main() {
     }
 
     for image in blend.instances_with_code(*b"IM") {
-        let image_name = image.get("id").get_string("name");
-        let mut cleaned_image_name = image_name.trim_start_matches("IM").to_owned();
+        let image_name = image.get_string("name");
 
-        for i in 1..=100 {
-            let image_suffix = format!(".{:03}", i);
-            if cleaned_image_name.ends_with(&image_suffix) {
-                cleaned_image_name = cleaned_image_name.trim_end_matches(&image_suffix).to_owned();
-                break;
-            }
+        if !image.fields.contains_key("packedfile") || !image_name.starts_with("//") {
+            println!("Skipping unexpected image instance");
+            continue;
         }
 
-        if image.fields.contains_key("packedfile") &&
-            (cleaned_image_name.ends_with(".jpg") || cleaned_image_name.ends_with(".jpeg")) {
+        let local_image_name = image_name.strip_prefix("//").unwrap();
+        let canonical_image_name = local_image_name.replace("\\", "/");
+        let image_filename = Path::new(&canonical_image_name)
+            .file_name()
+            .map(|filename| filename.to_string_lossy().into_owned())
+            .unwrap();
 
-            let image_packed_file = image.get("packedfile");
+        let image_packed_file = image.get("packedfile");
+        let image_data = image_packed_file.get_u8_vec("data");
 
-            if image_packed_file.fields.contains_key("data") {
-                let image_data = image_packed_file.get_u8_vec("data");
+        println!("Unpacking texture: {}", image_filename);
 
-                println!("Unpacking texture: {}", cleaned_image_name);
-
-                let mut image_file = File::create(format!("{}/{}", output_textures_dir, cleaned_image_name))
-                    .expect("Error creating texture image file");
-                image_file.write_all(&image_data)
-                    .expect("Error writing texture image file");
-
-            }
-
-        }
+        let mut image_file = File::create(format!("{}/{}", output_textures_dir, image_filename))
+            .expect("Error creating texture image file");
+        image_file.write_all(&image_data)
+            .expect("Error writing texture image file");
 
     }
 }
